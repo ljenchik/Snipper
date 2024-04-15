e986a1876605a531b38471c95b5e65f9d54b4475
ljenchik <pelagenkolena@gmail.com>
2024-04-15 21:48:26 +0100

    Added encryption and decryption for code snippets

--- routes/snippets.js
+++ routes/snippets.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = require("express").Router();
+const { encrypt, decrypt } = require("../utils/encrypt");
 const { Snippet } = require("../models");
 
 router.use(express.json());
@@ -7,19 +8,27 @@ router.use(express.urlencoded({ extended: true }));
 
 // get all snippets
 router.get("/", async (req, res, next) => {
-    let found_snippets = [];
-    if (req.query.lang) {
-        const lang = req.query.lang.toLocaleLowerCase();
-        found_snippets = await Snippet.findAll({
-            where: { language: lang },
-        });
-    } else {
-        found_snippets = await Snippet.findAll();
+    try {
+        let found_snippets = [];
+        if (req.query.lang) {
+            const lang = req.query.lang.toLocaleLowerCase();
+            found_snippets = await Snippet.findAll({
+                where: { language: lang },
+            });
+        } else {
+            found_snippets = await Snippet.findAll();
+        }
+        if (!found_snippets || found_snippets.length === 0) {
+            return res.status(404).json({ error: "Snippets not found" });
+        }
+        const decrypted_snippets = found_snippets.map((snippet) => ({
+            language: snippet.language,
+            code: decrypt(snippet.code),
+        }));
+        res.json(decrypted_snippets);
+    } catch (error) {
+        next(error);
     }
-    if (!found_snippets || found_snippets.length === 0) {
-        return res.status(404).json({ error: "Snippets not found" });
-    }
-    res.json(found_snippets);
 });
 
 // get a snippet by id
@@ -32,10 +41,32 @@ router.get("/:id", async (req, res, next) => {
     res.json(found_snippet);
 });
 
+// delete a snippet by id
+router.delete("/:id", async (req, res, next) => {
+    try {
+        const snippet_id = parseInt(req.params.id);
+        const found_snippet = await Snippet.findByPk(snippet_id);
+
+        if (!found_snippet) {
+            return res.status(404).json({ error: "Snippet not found" });
+        }
+
+        // Delete the snippet from the database
+        await found_snippet.destroy();
+
+        // Respond with a success message
+        res.json({ message: "Snippet deleted successfully" });
+    } catch (error) {
+        // Handle errors
+        next(error);
+    }
+});
+
 // create a new snippet
 router.post("/", async (req, res, next) => {
     try {
         req.body.language = req.body.language.toLocaleLowerCase();
+        req.body.code = encrypt(req.body.code);
         await Snippet.create(req.body);
         const snippets = await Snippet.findAll();
         res.send(snippets);
---
+++ utils/encrypt.js
@@ -0,0 +1,57 @@
+require("dotenv").config();
+const crypto = require("crypto");
+
+const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY;
+
+if (!ENCRYPTION_KEY) {
+    throw new Error("ENCRYPTION_KEY environment variable is missing.");
+}
+
+// the env variable ENCRYPTION_KEY contains 32 random bytes as hex
+// this is converted to a buffer to use as the secret key
+const key = Buffer.from(ENCRYPTION_KEY, "hex");
+
+// we are using the aes encryption algorithm
+const algo = "aes-256-cbc";
+
+function encrypt(plainText) {
+    // the Intialization Vector is random data which is mixed into the plainText before encryption
+    // it ensures that encrypting the same plainText twice does not result in the same
+    // encryptedText, which prevents attacks
+    const iv = crypto.randomBytes(16);
+    console.log(iv);
+
+    // cipher implements the aes algo with the key and IV
+    const cipher = crypto.createCipheriv(algo, key, iv);
+
+    // encrypt the text
+    let encryptedText = cipher.update(plainText, "utf8", "hex");
+    encryptedText += cipher.final("hex");
+
+    // we prefix the encryption with the IV because we need to use the same IV to decrypt
+    // it later on - it is ok for the IV to be public (but never the key)
+    return iv.toString("hex") + ":" + encryptedText;
+}
+
+function decrypt(input) {
+    console.log("input: ", input);
+    // split it into the IV which we prefixed
+    // and the text we want to decrypt
+    const parts = input.split(":");
+
+    // turn the IV into a buffer (it is currently hex)
+    const iv = Buffer.from(parts[0], "hex");
+    console.log("iv: ", iv);
+
+    // this is the encrypted text we want to decrypt again
+    const encryptedText = parts[1];
+
+    // decipher the text and format as utf-8 so we can read it!
+    const decipher = crypto.createDecipheriv(algo, key, iv);
+    let decryptedText = decipher.update(encryptedText, "hex", "utf8");
+    decryptedText += decipher.final("utf8");
+
+    return decryptedText;
+}
+
+module.exports = { encrypt, decrypt };
