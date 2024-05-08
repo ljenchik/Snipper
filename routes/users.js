423b6ffda9c7ec643d4c6813c7a831c2e78f0759
ljenchik <pelagenkolena@gmail.com>
2024-05-08 11:14:04 +0100

    Minor changes

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const { User } = require("../models");
+const bcrypt = require("bcrypt");
+const authorize = require("../utils/authorize");
 
 router.get("/", async (req, res) => {
     const all_users = await User.findAll();
@@ -8,6 +10,31 @@ router.get("/", async (req, res) => {
 
 router.get("/profile", (req, res, next) => {
     res.send(JSON.stringify(req.oidc.user));
+});
+
+router.post("/", async (req, res) => {
+    const all_users = await User.findAll();
+    res.json(all_users);
+});
+
+router.post("/", async (req, res) => {
+    const { email, password } = req.user;
+
+    // hash the password
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const user = {
+        id,
+        email,
+        password: hashedPassword,
+    };
+
+    // save the user
+    user.push(user);
+
+    // don't send back the hashed password
+    res.status(201).json({ id, email });
 });
 
 module.exports = router;
