67b6607a7d487ff02e24a5238da7dd4b67922b13
ljenchik <pelagenkolena@gmail.com>
2024-05-08 11:59:13 +0100

    Added outes for login and register

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const { User } = require("../models");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-const authorize = require("../utils/authorize");
+const check_user = require("../utils/check_user");
 
 router.get("/", async (req, res) => {
     const all_users = await User.findAll();
@@ -12,29 +13,77 @@ router.get("/profile", (req, res, next) => {
     res.send(JSON.stringify(req.oidc.user));
 });
 
-router.post("/", async (req, res) => {
-    const all_users = await User.findAll();
-    res.json(all_users);
+// router.post("/", async (req, res) => {
+//     const all_users = await User.findAll();
+//     res.json(all_users);
+// });
+
+// router.post("/", async (req, res) => {
+//     const { email, password } = req.user;
+
+//     // hash the password
+//     const saltRounds = 10;
+//     const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+//     const user = {
+//         id,
+//         email,
+//         password: hashedPassword,
+//     };
+
+//     // save the user
+//     user.push(user);
+
+//     // don't send back the hashed password
+//     res.status(201).json({ id, email });
+// });
+
+// register a user
+router.post("/register", async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res
+                .status(400)
+                .json({ error: "Email and password are required" });
+        }
+        const existing_user = await User.findOne({ where: { email } });
+        if (existing_user) {
+            return res.status(400).json({ error: "Email already exists" });
+        }
+        req.body.password = await bcrypt.hash(req.body.password, 3);
+        await User.create(req.body);
+        const users = await User.findAll();
+        res.send(users);
+    } catch (error) {
+        next(error);
+    }
 });
 
-router.post("/", async (req, res) => {
-    const { email, password } = req.user;
-
-    // hash the password
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-
-    const user = {
-        id,
-        email,
-        password: hashedPassword,
-    };
-
-    // save the user
-    user.push(user);
-
-    // don't send back the hashed password
-    res.status(201).json({ id, email });
+// login a user
+router.post("/login", check_user, async (req, res, next) => {
+    try {
+        const { email } = req.body;
+        const user = await User.findOne({
+            where: { email },
+        });
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+        const match = await bcrypt.compare(req.body.password, user.password);
+        if (match) {
+            const payload = { id: user.id, email: user.email };
+            const token = jwt.sign(payload, process.env.JWT_SECRET, {
+                expiresIn: "1w",
+            });
+            console.log("access token: ", token);
+            res.json({ message: "Success", token });
+        } else {
+            res.send("Failed");
+        }
+    } catch (error) {
+        next(error);
+    }
 });
 
 module.exports = router;
